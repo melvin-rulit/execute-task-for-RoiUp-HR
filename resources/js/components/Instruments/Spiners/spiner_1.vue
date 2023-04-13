e1a72c1ceb18a79936a858f613c3cfbcd05853d6
melvin-rulit <marchenko1985g@outlook.com>
2023-04-13 22:29:00 +0300

    add instrument

---
+++ resources/js/components/Instruments/Spiners/spiner_1.vue
@@ -0,0 +1,55 @@
+<template>
+
+<div class="regular_class">
+    <atom-spinner
+        animation-duration="2000"
+        :size="90"
+        :color="currentColor"
+    />
+</div>
+
+</template>
+
+<script>
+import {AtomSpinner, LoopingRhombusesSpinner, HollowDotsSpinner} from 'epic-spinners'
+
+export default {
+    components: {
+        AtomSpinner, LoopingRhombusesSpinner, HollowDotsSpinner
+    },
+    data() {
+        return {
+            currentColor: "",
+            colors: [
+                // "red",
+                "blue",
+                // "green",
+                // "indigo",
+                // "purple",
+                // "teal",
+                // "orange",
+                // "brown",
+                // "deep-orange",
+                // "blue-grey",
+                // "cyan",
+                // "white"
+            ],
+        }
+    },
+
+    mounted() {
+        this.randomColors()
+    },
+
+    methods: {
+        randomColors() {
+            this.currentColor =
+                this.colors[Math.floor(Math.random() * this.colors.length)];
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ resources/js/components/Instruments/Spiners/spiner_2.vue
@@ -0,0 +1,55 @@
+<template>
+
+<div class="regular_class">
+    <fingerprint-spinner
+        :animation-duration="1500"
+        :size="53"
+        color="#ff1d5e"
+    />
+</div>
+
+</template>
+
+<script>
+import {AtomSpinner, LoopingRhombusesSpinner, HollowDotsSpinner, FingerprintSpinner} from 'epic-spinners'
+
+export default {
+    components: {
+        AtomSpinner, LoopingRhombusesSpinner, HollowDotsSpinner, FingerprintSpinner
+    },
+    data() {
+        return {
+            currentColor: "",
+            colors: [
+                // "red",
+                "blue",
+                // "green",
+                // "indigo",
+                // "purple",
+                // "teal",
+                // "orange",
+                // "brown",
+                // "deep-orange",
+                // "blue-grey",
+                // "cyan",
+                // "white"
+            ],
+        }
+    },
+
+    mounted() {
+        this.randomColors()
+    },
+
+    methods: {
+        randomColors() {
+            this.currentColor =
+                this.colors[Math.floor(Math.random() * this.colors.length)];
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>

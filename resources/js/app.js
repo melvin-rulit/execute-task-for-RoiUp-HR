145297a51605ce89ba2ef6e54c9d33ebc093633e
melvin-rulit <marchenko1985g@outlook.com>
2023-04-14 01:59:49 +0300

    update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import './bootstrap';
 import BootstrapVue3 from 'bootstrap-vue-3'
-import VueTheMask from'vue-the-mask'
+import {LoadingPlugin} from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+import VueTheMask from 'vue-the-mask'
+
+
 
 import { createApp } from 'vue';
 import App from './components/App.vue';
@@ -15,15 +19,12 @@ import RegisterComponent from './components/Auth/Register.vue'
 import Spiner_1_Component from './components/Instruments/Spiners/spiner_1.vue'
 import Spiner_2_Component from './components/Instruments/Spiners/spiner_2.vue'
 
-//---------------------- NotificationComponent ----------------------------
-import Notification_1_Component from './components/Instruments/Notifications/notification_1.vue'
 
 //---------------------- MenuComponent and Sidebar ----------------------------
 import MenuComponent from './components/Menu/MenuComponent.vue'
 
 //---------------------- RouterComponents ----------------------------
 import HomePageComponent from './components/RouterComponents/HomePage.vue'
-// import DeleteLink from "./components/RouterComponents/DeleteLink.vue";
 
 
 
@@ -33,16 +34,14 @@ app.component('app_component', App);
 
 app.component('RegisterComponent', RegisterComponent);
 app.component('HomePageComponent', HomePageComponent);
-// app.component('DeleteLink', DeleteLink);
-
 app.component('MenuComponent', MenuComponent);
 
 app.component('Spiner_1', Spiner_1_Component);
 app.component('Spiner_2', Spiner_2_Component);
-app.component('Notification_1_Component', Notification_1_Component);
 
 app.use(BootstrapVue3)
 app.use(VueTheMask)
+app.use(LoadingPlugin)
 app.use(router)
 app.use(store)
 app.mount('#app');
